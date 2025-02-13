79ee89321de536aec30fb794e6ff83b3ac61a6c9
Imtiaj Bin Aoual <imtiajrex@gmail.com>
2025-02-14 02:48:31 +0600

    added exports

--- packages/core/src/components/ui/Form/Form.tsx
+++ packages/core/src/components/ui/Form/Form.tsx
@@ -1,11 +1,17 @@
-import { FormInput, FormPasswordInput, FormPinInput } from './FormInput';
+import * as FormInputs from './FormInput';
 import { wrapFormInputContainer } from './FormInputContainer';
-import { FormMultiSelect, FormSelect } from './FormSelect';
+import * as FormSelects from './FormSelect';
 
 export const Form = {
-  Input: wrapFormInputContainer(FormInput),
-  PinInput: wrapFormInputContainer(FormPinInput),
-  PasswordInput: wrapFormInputContainer(FormPasswordInput),
-  Select: wrapFormInputContainer(FormSelect),
-  MultiSelect: wrapFormInputContainer(FormMultiSelect),
+  Input: wrapFormInputContainer(FormInputs.FormInput),
+  PinInput: wrapFormInputContainer(FormInputs.FormPinInput),
+  PasswordInput: wrapFormInputContainer(FormInputs.FormPasswordInput),
+  Select: wrapFormInputContainer(FormSelects.FormSelect),
+  MultiSelect: wrapFormInputContainer(FormSelects.FormMultiSelect),
 };
+
+export const FormInput = wrapFormInputContainer(FormInputs.FormInput);
+export const FormPinInput = wrapFormInputContainer(FormInputs.FormPinInput);
+export const FormPasswordInput = wrapFormInputContainer(FormInputs.FormPasswordInput);
+export const FormSelect = wrapFormInputContainer(FormSelects.FormSelect);
+export const FormMultiSelect = wrapFormInputContainer(FormSelects.FormMultiSelect);
